6e771734c73ce5b872416971ddbdab46ab911f95
Mehmet Çelebi Gezer <58823004+gezerm85@users.noreply.github.com>
2024-09-23 17:07:23 +0300

    update

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.polygon.app',
+  appName: 'polygon-app',
+  webDir: 'dist/polygon-app',
+  bundledWebRuntime: false, 
+};
+
+export default config;
